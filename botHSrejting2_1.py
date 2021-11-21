0fa2bb51930a53dc7b674c20011b236698f81ba5
BEPb <57312267+BEPb@users.noreply.github.com>
2021-11-21 11:25:05 +0300

    21.11.2021 upd database

--- botHSrejting2_1.py
+++ botHSrejting2_1.py
@@ -5,9 +5,9 @@ import subprocess  # Запуск приложений windows
 import time  # работа со временем
 import keyboard  # работа с нажатиями клавиш
 
-# import bot800x600
+import bot800x600
 import voin_vild
-import mercenaries
+# import mercenaries
 
 def startlnk():  # функция запуска приложения
     subprocess.Popen('C:\Program Files (x86)\Battle.net\Battle.net Launcher.exe')  # запуск приложения
@@ -19,8 +19,9 @@ def startlnk():  # функция запуска приложения
 
 def main():
     while "Бесконечный цикл":  # Цикл анализа
-        voin_vild.voin_vild_deck()
-        # bot800x600.standart_game()
+        
+       # voin_vild.voin_vild_deck()
+        bot800x600.standart_game()
         # mercenaries.mercenaries_deck_mission_1_standart()
         # mercenaries.mercenaries_deck_mission_2_hero()
 
